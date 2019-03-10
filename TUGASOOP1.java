40ce05eddb54c6ca55129e45a1e8691d1e245f62
dita623 <48408512+dita623@users.noreply.github.com>
2019-03-10 20:57:37 +0700

    Add files via upload

---
+++ TUGASOOP1.java
@@ -0,0 +1,12 @@
+
+package tugasoop1;
+
+
+public class TUGASOOP1 {
+
+    public static void main(String[] args) {
+    System.out.println("SELAMAT DATANG DI OBJEK ORIENTIED PROGRAMING \n "
+            + "dita_ardiyani_17090016_4C");
+    }
+}
+    
